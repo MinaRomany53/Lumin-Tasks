d499bfb74ab0b4b67c0ea72c6dfcf0dfefaa253c
MinaRomany53 <minaromany190@gmail.com>
2023-08-13 01:58:19 +0300

    Build header component & create global style

---
+++ Lumin-Books/src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  isDarkTheme: boolean = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+
+    if (this.isDarkTheme) {
+      this.renderer.addClass(document.body, 'dark');
+      this.renderer.removeClass(document.body, 'light');
+    } else {
+      this.renderer.addClass(document.body, 'light');
+      this.renderer.removeClass(document.body, 'dark');
+    }
+  }
+}
